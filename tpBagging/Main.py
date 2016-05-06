4e6f150492d0cbf2729898be9b065b2a48b4f777
Matthieu Caron <matthieu.caron@laposte.net>
2016-05-06 16:11:17 +0200

    allez on ferme tu sors !"

--- tpBagging/Main.py
+++ tpBagging/Main.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 import numpy as np
 
 ##### DONNEES ##### 
@@ -20,14 +25,96 @@ def lecture(pathname) :
 (diabeteX,diabeteY,diabeteXtest,diabeteYtest) = lecture('pima-indians-diabetes.data')
 (spamX,spamY,spamXtest,spamYtest) = lecture('spambase.data')
 
+print "###### DecisionTreeClassifier ######"
 
-for prof in range(1,20) :
-    classifier = tree.DecisionTreeClassifier(max_depth=prof)
-    classifier = classifier.fit(diabeteX,diabeteY)
+for prof in range(1,10) :
+    TreeCL = tree.DecisionTreeClassifier(max_depth=prof)
+    TreeCL = TreeCL.fit(diabeteX,diabeteY)
     nbError = 0
-    tab = classifier.predict(diabeteXtest)
+    tab = TreeCL.predict(diabeteXtest)
     for i in range(len(diabeteXtest)) :
         if tab[i] != diabeteYtest[i] :
             nbError = nbError + 1
     tauxErr = float(nbError)/float(len(diabeteYtest))
     print "pour une profondeur de ",prof," voici le taux d erreur ",tauxErr
+
+print "###### RandomForestClassifier ######"
+
+for prof in range(1,10) :
+    RandomForestCL = RandomForestClassifier(n_estimators=10,max_depth=prof)
+    RandomForestCL = RandomForestCL.fit(diabeteX,diabeteY)
+    nbError = 0
+    tab = RandomForestCL.predict(diabeteXtest)
+    for i in range(len(diabeteXtest)) :
+        if tab[i] != diabeteYtest[i] :
+            nbError = nbError + 1
+        tauxErr = float(nbError)/float(len(diabeteYtest))
+    print "pour une profondeur de ",prof," voici le taux d erreur ",tauxErr
+
+print "###### ExtraTreesClassifier ######"
+
+for prof in range(1,20) :
+    ExtraTreeCL = ExtraTreesClassifier(n_estimators=10,max_depth=prof)
+    ExtraTreeCL = ExtraTreeCL.fit(diabeteX,diabeteY)
+    nbError = 0
+    tab = ExtraTreeCL.predict(diabeteXtest)
+    for i in range(len(diabeteXtest)) :
+        if tab[i] != diabeteYtest[i] :
+            nbError = nbError + 1
+        tauxErr = float(nbError)/float(len(diabeteYtest))
+    print "pour une profondeur de ",prof," voici le taux d erreur ",tauxErr
+
+
+print "####################################"
+print "###### Influence du nombre d arbre ######"
+print "####################################" 
+print "###### RandomForestClassifier ######"
+
+lesMoyennes = []
+lesVariances = []
+for arbre in range(5,20) :
+    lesTaux = []
+    for stat in range(30) :
+        RandomForestCL = RandomForestClassifier(n_estimators=arbre,max_depth=5)
+        RandomForestCL = RandomForestCL.fit(diabeteX,diabeteY)
+        nbError = 0
+        tab = RandomForestCL.predict(diabeteXtest)
+        for i in range(len(diabeteXtest)) :
+            if tab[i] != diabeteYtest[i] :
+                nbError = nbError + 1
+            tauxErr = float(nbError)/float(len(diabeteYtest))
+            lesTaux.append(tauxErr)
+    lesMoyennes.append((arbre,np.mean(lesTaux)))
+    lesVariances.append((arbre,np.var(lesTaux)))
+
+for (arbre,moyenne) in lesMoyennes :
+    print "La MOYENNE des erreur pour un nombre d arbre de  ",arbre," = ",moyenne
+    
+for (arbre,variance) in lesVariances :
+    print "La VARIANCE des erreur pour un nombre d arbre de ",arbre," = ",variance
+
+
+print "###### ExtraTreesClassifier ######"
+
+lesMoyennes = []
+lesVariances = []
+for arbre in range(5,20) :
+    lesTaux = []
+    for stat in range(30) :
+        ExtraTreeCL = ExtraTreesClassifier(n_estimators=arbre,max_depth=5)
+        ExtraTreeCL = ExtraTreeCL.fit(diabeteX,diabeteY)
+        nbError = 0
+        tab = ExtraTreeCL.predict(diabeteXtest)
+        for i in range(len(diabeteXtest)) :
+            if tab[i] != diabeteYtest[i] :
+                nbError = nbError + 1
+            tauxErr = float(nbError)/float(len(diabeteYtest))
+            lesTaux.append(tauxErr)
+    lesMoyennes.append((arbre,np.mean(lesTaux)))
+    lesVariances.append((arbre,np.var(lesTaux)))    
+
+for (arbre,moyenne) in lesMoyennes :
+    print "La MOYENNE des erreur pour un nombre d arbre de  ",arbre," = ",moyenne
+    
+for (arbre,variance) in lesVariances :
+    print "La VARIANCE des erreur pour un nombre d arbre de ",arbre," = ",variance
